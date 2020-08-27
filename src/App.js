532996989a8ace3153a1bbf92664b980194dbf79
maarcusf <mengo123>
2020-08-27 17:59:10 -0300

    Componentes e Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Equipe = () => {
+    return(
+        <a>Funcionou</a>
+    ) 
+}
+
+function App(){
+    return(
+    <div>
+       <h1>Conheça nossa equipe:</h1>
+        <Equipe nome = "Lucas" cargo= "Programador" idade=  "29"/>
+
+    </div>     
+    )
 }
 
 export default App;
